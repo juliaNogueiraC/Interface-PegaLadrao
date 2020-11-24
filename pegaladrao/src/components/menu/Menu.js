5c28375ca02ec949bfd3e5229460b9f4ffcf26ac
Matheus Magno <matheusmagno@MacBook-Air-de-Matheus.local>
2020-11-24 17:07:41 -0300

    menu updated

--- pegaladrao/src/components/menu/Menu.js
+++ pegaladrao/src/components/menu/Menu.js
@@ -4,7 +4,7 @@ import Icon from '../../img/Icon.png';
 function Menu() {
   return (
     <div class="topnav">
-      <img src={Icon} width="70" height="70"alt=""></img>
+      <img src={Icon} width="70" height="70" alt=""></img>
       <div class="topnav-right">
         <a class="active" href="#">Home</a>
         <a href="#">Registrar Ocorrência</a>
