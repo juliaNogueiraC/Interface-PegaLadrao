fe954dc567b748fd45121adabc253c5372c2052f
Matheus Magno <matheusmagno@MacBook-Air-de-Matheus.local>
2020-11-24 16:39:02 -0300

    Menu created

---
+++ pegaladrao/src/components/menu/Menu.js
@@ -0,0 +1,17 @@
+import './Menu.css';
+import Icon from '../../img/Icon.png';
+
+function Menu() {
+  return (
+    <div class="topnav">
+      <img src={Icon} width="70" height="70"alt=""></img>
+      <div class="topnav-right">
+        <a class="active" href="#">Home</a>
+        <a href="#">Registrar Ocorrência</a>
+        <a href="#">Ver ocorrências de um local</a>
+      </div>
+    </div>
+  );
+
+}
+export default Menu;
