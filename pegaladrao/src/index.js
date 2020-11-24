7532eb606d201333c69d454e3833d2edbb55a6ab
Matheus Magno <matheusmagno@MacBook-Air-de-Matheus.local>
2020-11-24 17:53:30 -0300

    added content home

--- pegaladrao/src/index.js
+++ pegaladrao/src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import Menu from './components/menu/Menu';
 
 
-ReactDOM.render( 
-    <Menu />,
+ReactDOM.render(
+  <div>
+    <Menu />
+    <div class="conteiner">
+      <div class="content">
+        <h1> Bem-Vindo(a)!</h1>
+        <p>
+          <em> Pega Ladrão </em> é um site de denúncia de ocorrências policiais.
+        Nós queremos informar para você o quão seguro ou não pode ser um determinado local .
+        Aqui você pode ir registrando todas as ocorrências que acontecerem com você, além de
+        ter acesso a ocorrência de outras pessoas.
+      </p>
+      </div>
+    </div>
+  </div>,
   document.getElementById('root')
 );
